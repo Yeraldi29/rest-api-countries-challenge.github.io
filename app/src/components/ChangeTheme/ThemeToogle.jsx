a9a916a9b07c2545f63c3e00c068141cabd65c61
Yeraldi <yeraldimujicarosales@gmaililil.com>
2022-08-13 18:24:30 -0400

    fixed dark mode and form

--- app/src/components/ChangeTheme/ThemeToogle.jsx
+++ app/src/components/ChangeTheme/ThemeToogle.jsx
@@ -6,13 +6,19 @@ import DarkModeIcon from '@mui/icons-material/DarkModeOutlined';
 const Toogle =()=>{
     const {theme, setTheme} = useContext(ThemeContext);
     return (
-        <div>
-            {theme === 'dark' ? (
-               <LightModeIcon sx={{color: 'white'}} className=" cursor-pointer"  onClick={()=>setTheme(theme=== 'dark' ? 'light' : 'dark')} />
-            ):(
-                <DarkModeIcon className=" cursor-pointer" onClick={()=>setTheme(theme=== 'dark' ? 'light' : 'dark')}/>
-            )}
-        </div>
+        <button className=' flex items-center py-2' onClick={()=>setTheme(theme=== 'dark' ? 'light' : 'dark')}>
+         {theme === 'dark' ? (
+            <>
+            <LightModeIcon sx={{color: 'white'}} className=" cursor-pointer"/>
+            <p className=' font-semibold dark:text-white pl-2'>Light Mode</p>
+            </>
+         ):(
+            <>
+            <DarkModeIcon className=" cursor-pointer"/>
+            <p className=' font-semibold dark:text-white pl-2'>Dark Mode</p>
+            </>
+          )}
+        </button>
     );
 }
 
--- app/src/components/Form.jsx
+++ app/src/components/Form.jsx
@@ -17,10 +17,10 @@ export default function Form(){
         <form className=" px-5  sm:px-9 sm:flex sm:justify-between md:px-14 lg:px-16 mt-6  "
          action="/" onSubmit={handleSubmit}>
             <div className="relative sm:w-80 lg:w-96 flex">
-              <input type="text"  placeholder="Search for a country..." className=" w-full shadow-Custom rounded-md h-[50px] pl-[85px] dark:bg-DarkBlue placeholder:text-VeryDarkBlue/30 placeholder:font-semibold   placeholder:dark:text-white/80 focus:outline-none" onChange={e => setName(e.target.value)} value={name} />
-              <button className="bg-Verylightwhite dark:bg-VeryDarkBlue text-VeryDarkBlue/30 dark:text-white/80 absolute top-[0.4rem] left-8 rounded-full h-10 w-10" onSubmit={handleSubmit}>
+              <input type="text"  placeholder="Search for a country..." className=" w-full shadow-Custom rounded-md h-[50px] pl-[85px] dark:bg-DarkBlue placeholder:text-VeryDarkBlue/30 placeholder:font-semibold   placeholder:dark:text-white/80 focus:outline-none" onChange={e => setName(e.target.value)} value={name}/>
+              <button type="submit" className="bg-Verylightwhite dark:bg-VeryDarkBlue text-VeryDarkBlue/30 dark:text-white/80 absolute top-[0.4rem] left-8 rounded-full h-10 w-10">
                   <SearchIcon className=" text-VeryDarkBlue/30 dark:text-white/80" sx={{fontSize:26}} />
-                  </button>
+              </button>
             </div>
             <SelectOptions/>
         </form>
